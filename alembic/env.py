70c98508caa078228a43629fd1b42fecf00c7348
d1sxrder3d <dan.lit612.wrk@gmail.com>
2026-07-06 20:31:29 +0300

    fix: Исправлена ошибка с sync-async драйверами у alembic, alembic.ini и env.py переписаны

--- alembic/env.py
+++ alembic/env.py
@@ -1,8 +1,10 @@
+import asyncio
 from logging.config import fileConfig
 
 from alembic import context
-from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+from sqlalchemy.ext.asyncio import async_engine_from_config
+
 from app.core.config import settings
 from app.models.base import BaseModel
 
@@ -15,6 +17,7 @@ if config.config_file_name is not None:
 target_metadata = BaseModel.metadata
 
 config.set_main_option("sqlalchemy.url", settings.db.alembic_url)
+
 
 def run_migrations_offline() -> None:
     url = config.get_main_option("sqlalchemy.url")
@@ -30,23 +33,27 @@ def run_migrations_offline() -> None:
         context.run_migrations()
 
 
-def run_migrations_online() -> None:
-    connectable = engine_from_config(
+def do_run_migrations(connection) -> None:
+    context.configure(connection=connection, target_metadata=target_metadata)
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_migrations_online() -> None:
+    connectable = async_engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
 
-    with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
 
-        with context.begin_transaction():
-            context.run_migrations()
+    await connectable.dispose()
 
 
 if context.is_offline_mode():
     run_migrations_offline()
 else:
-    run_migrations_online()
+    asyncio.run(run_migrations_online())
